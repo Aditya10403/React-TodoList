c1c43e5a8394f3bdd11134f084483df6007128e1
Aditya10403 <adityashukla@gmail.com>
2024-05-28 12:36:05 +0530

    update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import AS from "../assets/AS.png";
+import name from "../assets/name.png";
+import git from "../assets/git.svg";
 
 const NavBar = () => {
   return (
     <div>
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 dark:border-gray-600">
+        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://adityashukla-portfolio.vercel.app/" className="flex items-center rtl:space-x-reverse">
-            <img src="src/assets/AS.png" className="h-8" alt="Logo" />
-            <img src="src/assets/name.png" className="h-10 architects-daughter-regular self-center whitespace-nowrap dark:text-white" alt="name" />
+            <img src={AS} className="h-8" alt="Logo" />
+            <img src={name} className="h-10 architects-daughter-regular self-center whitespace-nowrap dark:text-white" alt="name" />
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><a href="https://github.com/Aditya10403"><img src="src/assets/git.svg" alt="" /></a></button>
+            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><a href="https://github.com/Aditya10403"><img src={git} alt="" /></a></button>
           </div>
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
